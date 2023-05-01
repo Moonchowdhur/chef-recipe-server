5e2bccc1566ece9e8c1539ed32faf74aa1b4bd5a
kaushani <kaushanichowdhury52@gmail.com>
2023-05-01 21:54:58 +0530

    chef3 added

--- index.js
+++ index.js
@@ -5,8 +5,20 @@ const port = process.env.port || 5000;
 
 app.use(cors());
 
+const chefs = require("./chefs.json");
+
 app.get("/", (req, res) => {
   res.send("Recipe is coming");
+});
+
+app.get("/chefs", (req, res) => {
+  res.send(chefs);
+});
+
+app.get("/chefs/:id", (req, res) => {
+  const id = req.params.id;
+  const chef = chefs.find((ch) => ch.id === id);
+  res.send(chef);
 });
 
 app.listen(port, () => {
